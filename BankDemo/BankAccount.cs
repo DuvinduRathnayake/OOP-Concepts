842defe5b0af04176f5574ea64da267df15ce2f7
Duvindu Rathnayake <duvindurathnayake77@gmail.com>
2025-09-26 06:39:27 -0400

    Complete Bankdemo code

---
+++ BankDemo/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDemo
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+        private readonly string _accountNumber;
+
+        public BankAccount(string accountNumber, decimal initialBalance = 0)
+        {
+            if (string.IsNullOrWhiteSpace(accountNumber))
+            {
+                throw new ArgumentException("Account number cannot be empty");
+            }
+
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Initial balance cannot be empty");
+            }
+
+            _accountNumber = accountNumber;
+            _balance = initialBalance;
+        }
+
+        public decimal Balance => _balance; // readonly property
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit must be a postive");
+            }
+
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawal amount should be positive");
+            }
+
+            if (amount > _balance)
+            {
+                throw new ArgumentException("Insufficient funds");
+            }
+            _balance -= amount;
+        }
+    }
+}
--- BankDemo/Program.cs
+++ BankDemo/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BankDemo;
+using System;
+
+namespace BankDemo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var account = new BankAccount("AC123", 100);
+            Console.WriteLine($"Account balance is:{account.Balance}");
+
+            account.Deposit(50);
+            Console.WriteLine($"After Deposit:{account.Balance}");
+
+            account.Withdraw(30);
+            Console.WriteLine($"After withdraw:{account.Balance}");
+
+            try
+            {
+                account.Withdraw(200); // should fail
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
